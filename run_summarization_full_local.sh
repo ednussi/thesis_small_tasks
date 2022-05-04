44f466fecf9e76275e06f33341e65001e9b965b7
Eran Nussinovitch <eran.nussinovitch@gmail.com>
2022-05-03 20:11:35 -0400

    sh runs

--- run_summarization_full_local.sh
+++ run_summarization_full_local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for d in 'xsum' 'samsum'
 do
-  for j in 42 43 44 45 46
+  for j in 42
   do
     for i in 1000000
     do
@@ -9,11 +9,12 @@ do
       do
         echo "=============== Dataset $d, Aug $aug, Examples $i, Seed $j ==============="
         EXPNAME="$d-$aug"
-        OUTPUTDIR="/d/Thesis/thesis_small_tasks/sumri_res_full/$EXPNAME/output-$i-$j"
+        EXPPATH="/d/Thesis/thesis_small_tasks/sumri_res_full/$EXPNAME"
+        OUTPUTDIR="$EXPPATH/output-$i-$j"
         mkdir -p -- $OUTPUTDIR
         python run_summarization.py --model_name_or_path t5-small --do_train --do_eval --dataset_name $d --source_prefix "summarize: " --output_dir $OUTPUTDIR --per_device_train_batch_size=4 --per_device_eval_batch_size=4 --overwrite_output_dir --predict_with_generate --warmup_ratio=0.1 --max_train_samples $i --num_train_epochs=10 --seed $j --aug $aug --save_steps=250000
-        rm -rf "$OUTPUTDIR/pytorch_model.bin"
-        rm -rf "$OUTPUTDIR/optimizer.pt"
+        rm -rf "$EXPPATH/pytorch_model.bin"
+        rm -rf "$EXPPATH/optimizer.pt"
         rm -rf "$OUTPUTDIR/checkpoint*/optimizer.pt"
         rm -rf "$OUTPUTDIR/checkpoint*/pytorch_model.bin"
       done
